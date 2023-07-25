33b9cd46d8713b8cb425c70cbd8ce3e63341b0dd
JohnMichaelMWong0411 <132361245+JohnMichaelMWong0411@users.noreply.github.com>
2023-07-25 23:04:42 +0800

    3rd commit

---
+++ src/Component/ServicePage/ServicePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ServicePage.css';
+
+const ServicePage = () => {
+  return (
+    <div id="ServicePage" className="ServicePage-container">
+            <div class="LGIImage">
+                <img src="./logoImg.jpeg" alt="Service Page"/>
+            </div>
+            <div class='LGIContent'>
+                <h1 class="primary-color">Logo & Brand Identity</h1> 
+                <h2>Work in harmony to create a strong and cohesive brand presence. 
+                    They communicate our brands values, differentiate us from competitors, 
+                    and resonate with our target audience. 
+                    Our logo and brand identity are the visual embodiment of our brands promise, 
+                    ensuring that every interaction with our brand leaves a lasting impression.</h2>
+            </div>
+      </div>
+  );
+};
+
+export default ServicePage;
