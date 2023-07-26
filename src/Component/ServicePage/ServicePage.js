a074f556737c9ab2a30187661b28075f1ddad515
JohnMichaelMWong0411 <132361245+JohnMichaelMWong0411@users.noreply.github.com>
2023-07-27 00:43:44 +0800

    7272023 commit

--- src/Component/ServicePage/ServicePage.js
+++ src/Component/ServicePage/ServicePage.js
@@ -4,18 +4,52 @@ import './ServicePage.css';
 const ServicePage = () => {
   return (
     <div id="ServicePage" className="ServicePage-container">
-            <div class='LGIContent'>
-                <div className='LGIContent-wrapper'>
-                <h1 class="primary-color">Logo & Brand Identity</h1> 
-                  <h2>Work in harmony to create a strong and cohesive brand presence. 
-                    They communicate our brands values, differentiate us from competitors, 
-                    and resonate with our target audience. 
-                    Our logo and brand identity are the visual embodiment of our brands promise, 
-                    ensuring that every interaction with our brand leaves a lasting impression.</h2>
-                </div>
+            <div className='header'>
+            <h1 class="primary-color">Logo & Brand Identity</h1> 
             </div>
-            <div class="LGIImage">
-                <img src="./logoImg.jpeg" alt="Service Page"/>
+            <div id="Logo" className="All-container">
+              <div class="All-Image">
+              <img src="./logoImg.jpeg" alt="Service Page"/>
+              </div>
+              <div class='All-Content'>
+              <h1 class="primary-color">Logo Designs</h1> 
+                    <h2>The key to a successful logo design is simplicity, versatility, and relevance to the brand's identity. It should be scalable to various sizes, from small applications like business cards to larger ones like billboards. Understanding the target audience and brand message is crucial in selecting the most appropriate style for a logo design.
+                  </h2>
+              </div>
+            </div>
+            <div id="Logo" className="All-container">
+              <div class="All-Image">
+              <img src="./logoImg.jpeg" alt="Service Page"/>
+              </div>
+              <div class='All-Content'>
+              <h1 class="primary-color">Business Card Designs</h1> 
+                    <h2>Should exude sophistication and professionalism while remaining visually captivating. The combination of elegant colors, refined typography, and well-organized information will leave a lasting impression on potential clients, making them more likely to reach out and engage with your business.
+                  </h2>
+              </div>
+            </div>
+
+            <div className='header'>
+            <h1 class="primary-color">Print Designs</h1> 
+            </div>
+            <div id="Logo" className="All-container">
+              <div class="All-Image">
+              <img src="./Brochureimg.jpg" alt="Service Page"/>
+              </div>
+              <div class='All-Content'>
+              <h1 class="primary-color">Brochure Designs</h1> 
+                    <h2>The key to a successful logo design is simplicity, versatility, and relevance to the brand's identity. It should be scalable to various sizes, from small applications like business cards to larger ones like billboards. Understanding the target audience and brand message is crucial in selecting the most appropriate style for a logo design.
+                  </h2>
+              </div>
+            </div>
+            <div id="Logo" className="All-container">
+              <div class="All-Image">
+              <img src="./Posterimg.jpg" alt="Service Page"/>
+              </div>
+              <div class='All-Content'>
+              <h1 class="primary-color">Poster Designs</h1> 
+                    <h2>Should exude sophistication and professionalism while remaining visually captivating. The combination of elegant colors, refined typography, and well-organized information will leave a lasting impression on potential clients, making them more likely to reach out and engage with your business.
+                  </h2>
+              </div>
             </div>
     </div>
   );
